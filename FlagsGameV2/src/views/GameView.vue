45ddecc8ed6a53e700e633cee4bbb6259f5b814e
Fabien Marceau <fabien.marceau@gmail.com>
2023-05-11 16:04:48 +0200

    ok

--- FlagsGameV2/src/views/GameView.vue
+++ FlagsGameV2/src/views/GameView.vue
@@ -32,7 +32,7 @@
           <img :src="countryFlag" alt="Image des drapeaux" class="countryFlag" ref="flag" />
           <div class="points">
             <h3 class="counter_title">Points</h3>
-            <h3 class="counter">{{ counter }}/10</h3>
+            <h3 class="counter" ref="Counter">{{ counter }}/10</h3>
           </div>
         </div>
         <div class="container_countries_names" ref="containerQuestionRandom">
@@ -47,8 +47,8 @@
         <div @click="restartGame" class="next" ref="nextButton">
           <i class="fa-solid fa-arrow-right fa-2xl arrow"></i>
         </div>
+        <button @click="reload" class="new_game" ref="reloadButton">Rejouer</button>
       </div>
-      <button @click="reload" class="new_game" v-if="showNewGameButton">Rejouer</button>
     </div>
   </div>
 </template>
@@ -74,8 +74,7 @@ export default {
       counter: 0,
       gamePart: 0,
       disabledDivs: false,
-      urlSelected: '',
-      showNewGameButton: false
+      urlSelected: ''
     }
   },
   methods: {
@@ -92,46 +91,64 @@ export default {
     },
     //fonction pour obtenir deux noms aléatoires qui seront associés au vrai nom de drapeau...................
 
+    // randomNameOne() {
+    //   this.flag.map(() => {
+    //     const randomIndex = Math.floor(Math.random() * this.flag.length)
+    //     const randomElement = this.flag[randomIndex]
+    //     this.dataNameRandomOne = randomElement.translations.fra.common
+    //     console.log(randomElement)
+    //   })
+    // },
+    // randomNameTwo() {
+    //   this.flag.map(() => {
+    //     const randomIndex = Math.floor(Math.random() * this.flag.length)
+    //     const randomElement = this.flag[randomIndex]
+    //     this.dataNameRandomTwo = randomElement.translations.fra.common
+    //   })
+    // },
+
     randomNameOne() {
-      this.flag.map(() => {
-        const randomIndex = Math.floor(Math.random() * this.flag.length)
-        const randomElement = this.flag[randomIndex]
-        // const randomElements = this.flag.sort(() => 0.5 - Math.random()).slice(0, 1)
-        this.dataNameRandomOne = randomElement.translations.fra.common
-        console.log(this.dataNameRandomOne)
-      })
+      const randomIndex = Math.floor(Math.random() * this.flag.length)
+      const randomElement = this.flag[randomIndex]
+      this.dataNameRandomOne = randomElement.translations.fra.common
     },
     randomNameTwo() {
-      this.flag.map(() => {
+      do {
         const randomIndex = Math.floor(Math.random() * this.flag.length)
         const randomElement = this.flag[randomIndex]
-        // const randomElements = this.flag.sort(() => 0.5 - Math.random()).slice(0, 1)
         this.dataNameRandomTwo = randomElement.translations.fra.common
-        console.log(this.dataNameRandomTwo)
-      })
+      } while (this.dataNameRandomOne === this.dataNameRandomTwo)
     },
+
     //  comportement des classes (chgmt couleur) suite à la réponse du User.................................
 
     reponse(event) {
       this.$refs.divGreen.classList.add('correct')
       this.$refs.divRedOne.classList.add('incorrect')
       this.$refs.divRedTwo.classList.add('incorrect')
+      this.$refs.divGreen.classList.add('disabled')
+      this.$refs.divRedOne.classList.add('disabled')
+      this.$refs.divRedTwo.classList.add('disabled')
       if (event.target.textContent == this.countryName) this.counter++
 
       //incrémenter à chaque clique des trois divs questions pour déterminer le nombre de coups par partie.......................................
 
-      console.log('ok')
       this.gamePart++
-      console.log(this.gamePart)
       if (this.gamePart === 10) {
-        console.log('Tu as fait une partie en 10')
+        this.$nextTick(() => {
+          this.$refs.reloadButton.scrollIntoView({
+            behavior: 'smooth',
+            block: 'end',
+            inline: 'nearest'
+          })
+        })
+        this.$refs.Counter.classList.add('counter_black')
+        this.$refs.reloadButton.classList.add('new_game_visible')
         this.$refs.divGreen.classList.add('disabled')
         this.$refs.divRedOne.classList.add('disabled')
         this.$refs.divRedTwo.classList.add('disabled')
         this.$refs.nextButton.classList.add('disabled')
         this.$refs.containerCounter.classList.add('container_counter_resultat')
-      
-        this.showNewGameButton = false;
       }
     },
     restartGame() {
@@ -139,6 +156,9 @@ export default {
       this.$refs.divGreen.classList.remove('correct')
       this.$refs.divRedOne.classList.remove('incorrect')
       this.$refs.divRedTwo.classList.remove('incorrect')
+      this.$refs.divGreen.classList.remove('disabled')
+      this.$refs.divRedOne.classList.remove('disabled')
+      this.$refs.divRedTwo.classList.remove('disabled')
       this.oneflag()
       this.randomNameOne()
       this.randomNameTwo()
@@ -151,16 +171,14 @@ export default {
       const parent = this.$refs.containerQuestionRandom
       const elements = parent.children
       for (let i = 0; i < elements.length; i++) {
-        parent.appendChild(elements[Math.floor(Math.random() * i)])
+        parent.appendChild(elements[Math.floor(Math.random() * elements.length)])
       }
     },
     findUrl(index) {
       let dataUrl = [this.urlA, this.urlB, this.urlC, this.urlD, this.urlE, this.urlF]
       this.urlSelected = dataUrl[index]
-      console.log(this.urlSelected)
       this.fetchData()
       this.$refs.flag.classList.add('countryFlag_visible')
-      console.log('ok')
       this.$refs.container_Gaming.classList.add('container_div_visible')
     },
     fetchData() {
@@ -300,7 +318,7 @@ export default {
   visibility: hidden;
 }
 .countryFlag_visible {
-  height: 180px;
+  height: 220px;
   width: 330px;
   box-shadow: 1px 1px 6px 0px rgb(69, 64, 64);
   border-radius: 2px;
@@ -353,10 +371,9 @@ export default {
   color: rgb(255, 255, 255);
   border-radius: 10px;
   box-shadow: 1px 1px 4px 0px rgb(4, 4, 4);
-  height: 120px;
+  height: 80px;
   width: 120px;
   border: none;
-  transition: visibility 2s ease-in;
   font-size: 18px;
   padding: 20px;
   border: solid white 2px;
@@ -376,9 +393,9 @@ export default {
   justify-content: space-around;
   padding-top: 30px;
   padding-bottom: 40px;
-  /* border: solid 1px orange; */
   border-radius: 7px;
   width: 100%;
+  z-index: 1;
 }
 .table_board h1 {
   color: white;
@@ -413,7 +430,7 @@ export default {
   gap: 5px;
 }
 .next {
-  background-color: blue;
+  background-color: #600dff;
   width: 60px;
   height: 60px;
   font-size: 14px;
@@ -445,16 +462,19 @@ export default {
 .arrow {
   color: white;
 }
+.counter_black {
+  background-color: black;
+}
 
-/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+/* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 
 @media screen and (max-width: 768px) {
   .container_game {
     margin-top: 10px;
   }
-.countries_names{
-  font-size: 14px;
-}
+  .countries_names {
+    font-size: 14px;
+  }
   .countryFlag_visible {
     height: 100px;
     width: 100px;
@@ -468,7 +488,7 @@ export default {
   .counter {
     display: flex;
     align-items: left;
-    justify-content: left;
+    justify-content: center;
     font-size: 14px;
     color: rgb(255, 255, 255);
     border: solid 1px white;
@@ -487,7 +507,7 @@ export default {
     gap: 15px;
   }
   .countryFlag_visible {
-    height: 120px;
+    height: 110px;
     width: 150px;
     box-shadow: 1px 1px 6px 0px rgb(69, 64, 64);
     border-radius: 2px;
@@ -506,6 +526,7 @@ export default {
     width: 90%;
     height: 30px;
     z-index: 1;
+    margin-top: 10px;
   }
   .continent {
     color: orangered;
@@ -524,13 +545,40 @@ export default {
     margin-top: 0px;
   }
   .container_div_visible {
-    margin-top: 50px;
+    margin-top: 70px;
     display: flex;
     flex-direction: column;
     align-items: center;
     visibility: visible;
     width: 100%;
     gap: 20px;
+  }
+}
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+@media screen and (min-width: 768px) {
+  .container_countries_names {
+    width: 400px;
+  }
+  .next {
+    background-color: #600dff;
+    width: 60px;
+    height: 60px;
+    font-size: 14px;
+    border-radius: 30%;
+    border: 1px solid rgb(53, 50, 50);
+    transition: 0.4s ease-in-out;
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    box-shadow: 1px 1px 5px rgb(23, 23, 23);
+    transition: 0.1s ease-in-out;
+  }
+  .table_board {
+    margin-top: 5px;
+    box-shadow: 2px -1px 5px black inset, -2px 1px 5px black inset;
   }
 }
 </style>
